45c8d9d092c8affc2e0ce5a50aa7ec62a35440d4
George Kaimakis <geoptus@gmail.com>
2018-03-01 22:00:02 +0000

    Adds 'app.py' basic flask web app script:
    This simple web app script links three web pages together in a
    circular fashion.
    There are as yet no restrictions, security or verification.
    
    Run 'python app.py' from the terminal to start the flask web sever.
    Call 'localhost:5000' on a web browser to see the pages.
    Use 'username: admin' and 'password: admin' on the login page.
    Use wrong credentials to see the response.

--- app.py
+++ app.py
@@ -7,25 +7,32 @@ from flask import Flask, render_template, redirect, url_for, request
 # create the application object:
 app = Flask(__name__)
 
+
 # use decorators to link the function to a url:
 @app.route('/')
 def home():
-    # return "Hello, world!"
+    """calls the web site home page"""
     return render_template("index.html")
+
 
 @app.route('/welcome')
 def welcome():
+    """calls the web site welcome page"""
     return render_template("welcome.html")
+
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    """calls the web site login page"""
     error = None
     if request.method == 'POST':
-        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+        if request.form['username'] != 'admin' or \
+                request.form['password'] != 'admin':
             error = 'Invalid credentials. Please try again.'
         else:
             return redirect(url_for('welcome'))
     return render_template('login.html', error=error)
+
 
 # start the sever with the 'run()' method:
 if __name__ == '__main__':
